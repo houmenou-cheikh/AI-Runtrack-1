5f47bbc9efdc6aa9469ce05970462d0b57001a8e
houmenou-cheikh <cheikh-tidiane.houmenou@laplateformeio>
2020-10-21 18:34:34 +0200

    Runtrack1-jour1: modif Job37

--- Job_jour1/Job37.py
+++ Job_jour1/Job37.py
@@ -3,28 +3,37 @@ import string
 def newWord():
     
     oldWord = input("Veuillez saisir un nouveau mot: ")
+    halfWord = round(len(oldWord) / 2) 
+    socle = oldWord[0:halfWord] 
+ 
     alphabet = list(string.ascii_lowercase)
     dicoAlphabet = {}
     InvDicoAphabet = {}
+    
     for postion, letter in enumerate(alphabet):
         dicoAlphabet[letter] = postion
         InvDicoAphabet[postion] = letter
 
+    
     listeNewWord = []
     listeOfoldWord = oldWord.split(" ")
+    
     for lettre in oldWord:
         if len(listeOfoldWord)> 1 or lettre not in alphabet:
-            oldWord = input("**Ce mot contient un caractere incorrecte,Veuillez saisir un seul mot**: ")
-
-        else:
-            listeNewWord.append(dicoAlphabet[lettre])
+            print("**Ce mot contient un caractere incorrecte,Veuillez saisir un seul mot**: ")
+            return 1
+		
+     
+    for let in oldWord[halfWord: ]:
+         listeNewWord.append(dicoAlphabet[let])
         
     NewWord = ''           
     listeOrdonnee = sorted(listeNewWord)
     for pos in listeOrdonnee:
         NewWord = NewWord + InvDicoAphabet[pos]
-
-    print(NewWord)
+    
+    NewWord = socle + NewWord
+    print('le nouveau mot est:  ',NewWord)
 
 
 newWord()
